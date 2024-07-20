260df676cef2232a0a4fbc62a4a7353a76b4909d
KshitijAucharmal <kshitijaucharmal21@gmail.com>
2024-07-20 14:40:50 -0700

    merge

---
+++ Assets/Sciripts/PlayerMovement.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float rotationSpeed = 10f;
+    [SerializeField] private int maxPoints = 50;
+    [SerializeField] private float maxdistpoints = 0.1f;
+    [SerializeField] private string p_horizontal;
+    [SerializeField] private string p_vertical;
+
+    private Vector3 lastPosition;
+    private LineRenderer lineRenderer;
+    [SerializeField] private Quaternion offsetRot = Quaternion.Euler(0, -90f, 0);
+
+    [HideInInspector] public Queue<Vector3> positions = new Queue<Vector3>();
+    [HideInInspector] public bool canUsePowerup = true;
+   // Health health;
+
+    void Start()
+    {
+       // health = new Health(100);
+        lastPosition = transform.position;
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 1;
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
+        lineRenderer.material.color = Color.black;
+        lineRenderer.useWorldSpace = true;
+        lineRenderer.SetPosition(0, transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxisRaw(p_horizontal);
+        float moveVertical = Input.GetAxisRaw(p_vertical);
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical).normalized;
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+
+        if (movement == Vector3.zero)
+        {
+            // canUsePowerup = true;
+            return;
+        }
+        // canUsePowerup = false;
+
+        Quaternion targetRotation = Quaternion.LookRotation(movement, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation * offsetRot, rotationSpeed * Time.deltaTime);
+
+
+
+        if (Vector3.Distance(transform.position, lastPosition) > maxdistpoints)
+        {
+            lastPosition = transform.position;
+            positions.Enqueue(transform.position);
+            if (positions.Count > maxPoints)
+            {
+                positions.Dequeue();
+            }
+            lineRenderer.positionCount = positions.Count;
+            lineRenderer.SetPositions(positions.ToArray());
+        }
+
+    }
+
+    public void ResetPositions()
+    {
+        positions.Clear();
+        lineRenderer.positionCount = 0;
+    }
+}
---
+++ Assets/Sciripts/Powerups/Circle_Powerup.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Circle_Powerup : MonoBehaviour {
+
+    [SerializeField] private float explosionTime = 0.5f;
+    [SerializeField] private float force = 200f;
+    [SerializeField] private ParticleSystemRenderer colorParticles;
+
+    [SerializeField] private Material player1Mat;
+    [SerializeField] private Material player2Mat;
+
+    [Header("Explosion Settings")]
+    [SerializeField] private float range = 5f;
+
+    private int playerN;
+
+    public void SetPlayer(int playerNo){
+        playerN = playerNo;
+        colorParticles.material = playerNo == 1 ? player1Mat : player2Mat;
+    }
+
+    // Start is called before the first frame update
+    void Start() {
+        Destroy(gameObject, explosionTime);
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, range);
+        foreach(Collider nearObj in colliders){
+            if(nearObj.CompareTag("Player" + playerN))
+            {
+                continue;
+            }
+            else if(nearObj.CompareTag("Player" + (playerN == 1 ? 2 : 1))){
+                var opprb = nearObj.GetComponent<Rigidbody>();
+                opprb.AddForce(transform.up * force, ForceMode.Impulse);
+            }
+            Rigidbody rb = nearObj.GetComponent<Rigidbody>();
+            if(rb != null){
+                rb.AddExplosionForce(force, transform.position, range);
+            }
+        }
+    }
+
+    void OnDrawGizmos(){
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+    }
+
+    void FixedUpdate(){
+        
+    }
+}
